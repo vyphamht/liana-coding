7074d93f11d318caad7c84033d027a15d0026697
phamhavy171 <59789678+phamhavy171@users.noreply.github.com>
2021-02-14 15:43:24 +0200

    update functions

--- index.js
+++ index.js
@@ -1,1 +1,158 @@
+const RSS_URL = "./feed.xml";
 
+fetch(RSS_URL)
+  .then((response) => response.text())
+  .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+  .then((data) => {
+    console.log(data);
+    const items = data.querySelectorAll("item");
+    let html = ``;
+    items.forEach((el) => {
+      html += `
+        <div class="news-card">
+        <h6 class="text-muted" style="font-size: 12px">${el
+          .querySelector("pubDate")
+          .innerHTML.slice(5, 17)}</h6>
+          <p>
+            <a href="${
+              el.querySelector("link").innerHTML
+            }" target="_blank" rel="noopener">
+              ${el.querySelector("title").innerHTML}
+            </a>
+          </p>
+        </div>
+      `;
+    });
+    document.getElementById("feedarea").innerHTML = html;
+  });
+
+// counter
+(function ($) {
+  $.fn.countTo = function (options) {
+    options = options || {};
+
+    return $(this).each(function () {
+      // set options for current element
+      var settings = $.extend(
+        {},
+        $.fn.countTo.defaults,
+        {
+          from: $(this).data("from"),
+          to: $(this).data("to"),
+          speed: $(this).data("speed"),
+          refreshInterval: $(this).data("refresh-interval"),
+          decimals: $(this).data("decimals"),
+        },
+        options
+      );
+
+      // how many times to update the value, and how much to increment the value on each update
+      var loops = Math.ceil(settings.speed / settings.refreshInterval),
+        increment = (settings.to - settings.from) / loops;
+
+      // references & variables that will change with each update
+      var self = this,
+        $self = $(this),
+        loopCount = 0,
+        value = settings.from,
+        data = $self.data("countTo") || {};
+
+      $self.data("countTo", data);
+
+      // if an existing interval can be found, clear it first
+      if (data.interval) {
+        clearInterval(data.interval);
+      }
+      data.interval = setInterval(updateTimer, settings.refreshInterval);
+
+      // initialize the element with the starting value
+      render(value);
+
+      function updateTimer() {
+        value += increment;
+        loopCount++;
+
+        render(value);
+
+        if (typeof settings.onUpdate == "function") {
+          settings.onUpdate.call(self, value);
+        }
+
+        if (loopCount >= loops) {
+          // remove the interval
+          $self.removeData("countTo");
+          clearInterval(data.interval);
+          value = settings.to;
+
+          if (typeof settings.onComplete == "function") {
+            settings.onComplete.call(self, value);
+          }
+        }
+      }
+
+      function render(value) {
+        var formattedValue = settings.formatter.call(self, value, settings);
+        $self.html(formattedValue);
+      }
+    });
+  };
+
+  $.fn.countTo.defaults = {
+    from: 0, // the number the element should start at
+    to: 0, // the number the element should end at
+    speed: 1000, // how long it should take to count between the target numbers
+    refreshInterval: 100, // how often the element should be updated
+    decimals: 0, // the number of decimal places to show
+    formatter: formatter, // handler for formatting the value before rendering
+    onUpdate: null, // callback method for every time the element is updated
+    onComplete: null, // callback method for when the element finishes updating
+  };
+
+  function formatter(value, settings) {
+    return value.toFixed(settings.decimals);
+  }
+})(jQuery);
+
+jQuery(function ($) {
+  // custom formatting example
+  $(".count-number").data("countToOptions", {
+    formatter: function (value, options) {
+      return value
+        .toFixed(options.decimals)
+        .replace(/\B(?=(?:\d{3})+(?!\d))/g, ",");
+    },
+  });
+
+  // start all the timers
+  $(".timer").each(count);
+
+  function count(options) {
+    var $this = $(this);
+    options = $.extend({}, options || {}, $this.data("countToOptions") || {});
+    $this.countTo(options);
+  }
+});
+
+// email subscription
+var validateEmail = function (elementValue) {
+  var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailPattern.test(elementValue);
+};
+$("#email").keyup(function () {
+  var value = $(this).val();
+  var valid = validateEmail(value);
+
+  if (!valid) {
+    $(this).css("color", "red");
+    $(".subbtn").prop("disabled", true);
+  } else {
+    $(this).css("color", "#4c8930");
+    $(".subbtn").prop("disabled", false);
+  }
+});
+
+function subcribe() {
+  console.log("click");
+  document.querySelector("#successful-sub").classList.remove("hide");
+  document.querySelector(".main-subscription-form").style.display = "none";
+}
